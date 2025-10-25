1db660897cb6bf2511ce5506a6f325eb05d9241d
Thierry Soreze <thierrysoreze@hotmail.com>
2025-10-25 23:01:48 +0200

    Delete ATLAS directory

--- ATLAS/atlas/solvers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .schrodinger_bridge import SchroedingerBridgeSolver
-from .hierarchical_sampler import AdvancedHierarchicalDiffusionSampler
-
-__all__ = ['SchroedingerBridgeSolver', 'AdvancedHierarchicalDiffusionSampler']
-
