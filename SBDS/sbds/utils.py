8daf02dd5cc80141cc134ffaab3eb167d4ee4d61
Thierry Soreze <thierrysoreze@hotmail.com>
2025-11-26 11:36:35 +0100

    Fix spectral gradient spatial dimension handling

--- SBDS/sbds/utils.py
+++ SBDS/sbds/utils.py
@@ -35,7 +35,7 @@ def spectral_gradient(
     if u.dim() < 1:
         raise ValueError("Input tensor must have at least one dimension")
 
-    spatial_dims = list(range(-min(2, u.dim()), 0)) if u.dim() >= 2 else [-1]
+    spatial_dims = [-1] if u.dim() <= 2 else list(range(-2, 0))
     spatial_shape = [u.size(d) for d in spatial_dims]
     ndim = len(spatial_dims)
 
